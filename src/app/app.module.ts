8184041174f719a1ebe728d9406b2991cfdd8790
Benjamin Ikeh <bikeh@quantumgroupgh.com>
2021-07-29 09:36:41 +0000

    reset and commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DefaultComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
