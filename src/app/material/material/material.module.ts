4c71a6f6e51f7ff611961988789214e2a2e484ea
Benjamin Ikeh <bikeh@quantumgroupgh.com>
2021-07-30 16:43:34 +0000

    add material modules

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from "@angular/material/button";
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const material = [
-  MatSidenavModule
+  MatSidenavModule,
+  MatDividerModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  FlexLayoutModule
 ]
 
 @NgModule({
