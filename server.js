33b78a449f673e7931a3dc4a322a89cf5dd023e5
Benjamin Ikeh <bikeh@quantumgroupgh.com>
2021-08-09 15:49:41 +0000

    host heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+//install express server
+
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+//serve only the static files from the dist directory
+app.use(express.static("./dist/admin-dashboard-panel"));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: 'dist/admin-dashboard-panel'});
+});
+
+app.listen(process.env.PORT || 8765);
